5b32b413a151a0ac97c49427cb8bb9246a807a6e
narenaruiz <narena@cifpfbmoll.eu>
2020-03-31 16:17:06 +0200

    Acabando, me falta las excepciones

---
+++ src/practica/pkg7/ErrorRutaEntrada.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica.pkg7;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ErrorRutaEntrada {
+    
+}
