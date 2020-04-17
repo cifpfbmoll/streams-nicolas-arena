5f55502ab8ff4223529e598a3215f72317069e60
narenaruiz <narena@cifpfbmoll.eu>
2020-04-17 19:04:02 +0200

    he empezado el ejercicio 2(el de objetos)

--- src/practica/pkg7/CarteleraObjetos.java
+++ src/practica/pkg7/CarteleraObjetos.java
@@ -5,10 +5,38 @@
  */
 package practica.pkg7;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 /**
  *
  * @author Usuario
  */
 public class CarteleraObjetos {
-    
+
+    public static void leerEscribirObjetos(String rutaOrigen, String rutaDestino) throws FileNotFoundException, IOException {
+        ObjectInputStream lectura = null;
+        File origen = new File(rutaOrigen);
+        File destino = new File(rutaDestino);
+        FileInputStream entrada = new FileInputStream(origen);
+        FileOutputStream salida = new FileOutputStream(destino);
+        lectura = new ObjectInputStream(entrada);
+        ObjectOutputStream escribir = new ObjectOutputStream(salida);
+        lectura.close();
+        escribir.close();
+
+    }
+
+    /* No se si hacer uno para cada cosa o ponerlo junto todo
+    public static void leerObjetos(String rutaOrigen) {
+    }
+
+    public static void escribirObjetos(String rutaDestino) {
+    }
+     */
 }
